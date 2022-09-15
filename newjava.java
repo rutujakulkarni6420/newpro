0a9ad3f94a164bd7a0697336f3e3d2c853eb9a00
raj <raj@gmail.com>
2022-09-15 10:54:13 +0000

    new

--- newjava.java
+++ newjava.java
@@ -1,7 +1,7 @@
 class GFG {
     public static void main(String[] args)
     {
-        for (int i = 1; i <= 12; i++) {
+        for (int i = 1; i <= 16; i++) {
             System.out.println(i);
         }
     }
